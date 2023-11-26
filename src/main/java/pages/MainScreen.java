926f2dbe469b2e5d3b108f728d6d260ca43f04ab
Sara_Salama <sarasa200011@gmail.com>
2023-11-26 08:51:04 +0200

    Changing Time format from 12 to 24 hours

--- src/main/java/pages/MainScreen.java
+++ src/main/java/pages/MainScreen.java
@@ -1,14 +1,18 @@
 package pages;
 
+import com.beust.ah.A;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.PrintsPage;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 
 import java.time.Duration;
+import java.util.List;
 
 public class MainScreen {
 
@@ -18,6 +22,11 @@ public class MainScreen {
     private final By privacyPolicy = AppiumBy.id("com.info.weather.forecast:id/ll_got_it");
     private final By temperatureUnit = AppiumBy.id("com.info.weather.forecast:id/tv_current_temper_unit");
     private final By tvDateId = AppiumBy.id("com.info.weather.forecast:id/tv_date");
+    private final By timeFormatDropDownId = AppiumBy.id("com.info.weather.forecast:id/iv_timeformat_dropdown");
+    private final By timeFormat24Hour = AppiumBy.xpath("//android.widget.FrameLayout[@index='2']//android.widget.TextView[@index='1']");
+    private final By timeFormatSetting = AppiumBy.id("com.info.weather.forecast:id/tv_timeformat_setting");
+    private final By navigationSetting = AppiumBy.id("com.info.weather.forecast:id/iv_bt_navigation_setting");
+    private final By unitSettingId = AppiumBy.id("com.info.weather.forecast:id/ll_item_unit_setting");
 
 
     public MainScreen(AppiumDriver driver){
@@ -29,6 +38,25 @@ public class MainScreen {
         driver.findElement(settingDoneButton).click();
         wait.until(ExpectedConditions.elementToBeClickable(privacyPolicy)).click();
 
+    }
+
+    public void openNavigationSettings(){
+        driver.findElement(navigationSetting).click();
+    }
+
+    public void selectUnitSettings(){
+        driver.findElement(unitSettingId).click();
+    }
+
+
+    public void changeTimeFormatTo24hours(){
+        driver.findElement(timeFormatDropDownId).click();
+        driver.findElement(timeFormat24Hour).click();
+        String timeFormat = driver.findElement(timeFormatSetting).getText();
+
+        Assert.assertEquals(timeFormat, "24 Hour");
+
+        driver.findElement(settingDoneButton).click();
     }
 
     public void assert12HoursFormat(){
@@ -56,6 +84,5 @@ public class MainScreen {
 
         Assert.assertEquals(tempUnit, "°F");
     }
-
 
 }
--- src/test/java/tests/testScenarios.java
+++ src/test/java/tests/testScenarios.java
@@ -4,9 +4,18 @@ import org.testng.annotations.Test;
 
 public class testScenarios extends baseTest{
 
-    @Test
-    public void first(){
+    @Test(priority = 1)
+    public void getMainScreenWith12HoursFormatAndFahrenheitUnit(){
         mainScreen.getMainScreen();
+        mainScreen.assertTempUnitInFahrenheit();
+        mainScreen.assert12HoursFormat();
+    }
 
+    @Test(priority = 2)
+    public void changeTemperatureUnitToCelsius(){
+        mainScreen.openNavigationSettings();
+        mainScreen.selectUnitSettings();
+        mainScreen.changeTimeFormatTo24hours();
+        mainScreen.assert24HoursFormat();
     }
 }
